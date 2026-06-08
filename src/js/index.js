347ed0b1a59ce5e6db735265c7d02d2be0e3057d
viju <avijusanjai@gmail.com>
2026-06-09 02:48:18 +0530

    index.js: export exec and wasmpeg
    Signed-off-by: viju <avijusanjai@gmail.com>

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,4 @@
-export { FFmpeg } from './ffmpeg.js';
-export { gpu }    from './gpu.js';
+export { FFmpeg }  from './ffmpeg.js';
+export { gpu }     from './gpu.js';
+export { exec }    from './exec.mjs';
+export { default as wasmpeg } from './wasmpeg.mjs';
