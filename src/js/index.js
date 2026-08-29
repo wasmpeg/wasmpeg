0caba4bb980ccd96abad3334f96719408937d0d8
viju <avijusanjai@gmail.com>
2026-08-29 21:17:52 +0530

    fix: add the default export the README documents
    Signed-off-by: viju <avijusanjai@gmail.com>

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,6 @@ export { FFmpeg }  from './ffmpeg.js';
 export { gpu }     from './gpu.js';
 export { exec }    from './exec.mjs';
 export { default as wasmpeg } from './wasmpeg.mjs';
+
+// The documented entry point: `import wasmpeg from 'wasmpeg'`.
+export { default } from './wasmpeg.mjs';
